f7a9848c9217456e35aaf5caa1900aaa012164d7
geewailee <30604942+geewailee@users.noreply.github.com>
2020-11-25 15:38:02 -0500

    added Navbar

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 import Home from "./components/Home.jsx";
+import Navbar from "./components/Navbar.jsx";
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path='/'>
           <Home />
